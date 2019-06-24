bdc1f9bf9499b7c754f8e433f7f2b2fd59efe9fd
ChenKuo <ckuo1@umbc.edu>
2019-06-24 18:34:32 -0400

    search

--- index.js
+++ index.js
@@ -4,19 +4,36 @@ function setBool(index, option){
     var orEle = element.getElementsByClassName('or')[0]
     var notEle = element.getElementsByClassName('not')[0]
     if (option === 'and'){
-        andEle.classList.add('active')
-        orEle.classList.remove('active')
-        notEle.classList.remove('active')
+        if(andEle.classList.contains('active'))
+            andEle.classList.remove('active')
+        else{
+            andEle.classList.add('active')
+            orEle.classList.remove('active')
+            notEle.classList.remove('active')
+        }
     }
     else if (option === 'or'){
-        andEle.classList.remove('active')
-        orEle.classList.add('active')
-        notEle.classList.remove('active')
+        if(orEle.classList.contains('active'))
+            orEle.classList.remove('active')
+        else{
+            andEle.classList.remove('active')
+            orEle.classList.add('active')
+            notEle.classList.remove('active')
+        }
     }
     else if (option === 'not'){
+        if(notEle.classList.contains('active'))
+            notEle.classList.remove('active')
+        else{
+            andEle.classList.remove('active')
+            orEle.classList.remove('active')
+            notEle.classList.add('active')
+        }
+    }
+    else if(option === 'clear'){
         andEle.classList.remove('active')
         orEle.classList.remove('active')
-        notEle.classList.add('active')
+        notEle.classList.remove('active')
     }
     return false
 }
@@ -24,6 +41,9 @@ function setBool(index, option){
 function clearForm(){
     var form = document.getElementById("form");   
     form.reset();
+    setBool(1,'clear');
+    setBool(2,'clear');
+    setBool(3,'clear');
 }
 
 function search(){
